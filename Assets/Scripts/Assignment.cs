f9150cafe79dc6043a65110775a2f31ae8b01621
LemonAndMint <orn.betul@gmail.com>
2023-08-26 13:57:07 +0300

    Initial commit

---
+++ Assets/Scripts/Assignment.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment : MonoBehaviour
+{
+
+    int score;
+    float 
+    
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+}
